904a41a94f75563331454752c64259e9dcbdcf4b
kim <kim@kims-iMac.local>
2023-05-30 06:29:15 +0900

    1번 빼고는 다 인덱스가 리스트의 범위를 벗어놨다고 뜨지만 클릭은 다시 다 된다.

--- 0529_testchat.py
+++ 0529_testchat.py
@@ -64,38 +64,51 @@ print('[크롤링 시작...]')
 # 장소 리스트
 parking_list = driver.find_elements(By.CSS_SELECTOR, 'li.DWs4Q')
 print(parking_list)
-names = []       #병원명
-#[<selenium.webdriver.remote.webelement.WebElement (session="bdea8eeab0f544ba4b7bed03e2e30ace", element="9E2D84DE1F26E97B6AD3EA3FE59FB171_element_130")>]
-for data in range(len(parking_list)):  #장소 리스트 만큼
-    print(data)
 
-    sleep(3)
+#[<selenium.webdriver.remote.webelement.WebElement (session="bdea8eeab0f544ba4b7bed03e2e30ace", element="9E2D84DE1F26E97B6AD3EA3FE59FB171_element_130")>]
+for index, data in enumerate(parking_list, start=0):  #장소 리스트 만큼
+    
+    print(index+1)
+
+    sleep(5)
     try:
         # (1) 상세정보 버튼 누르기 
-        driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/ul/li[{}]/div[2]/a[1]/div[1]/div/span[1]'.format(data)).click()
-        sleep(1)
+        driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/ul/li[{}]/div[2]/a[1]/div[1]/div/span[1]'.format(index+1)).click()
+        sleep(3)
 
         #프레임전환
         switch_frame('entryIframe')
         sleep(2)
 
-        name = driver.find_elements(By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div[1]/div[1]/span[1]')
-        sleep(2)
-        names.append(name.text)
-        
-        #프레임전환
+        names = driver.find_elements(By.CSS_SELECTOR, '.Fc1rA')  # (3) 장소명
+        if index < len(names):
+            name = names[index].text
+            print(name)
+
+            # dict에 데이터 집어넣기
+            dict_temp = {
+                'ho': names
+            }
+            parking_dict['병원정보'].append(dict_temp)
+            print(f'{name}...완료')
+            sleep(2)
+        else:
+            print(f"인덱스 {index}는 names 리스트의 범위를 벗어났습니다.")
+            sleep(2)
+
+        # 프레임 전환
         switch_frame('searchIframe')
         sleep(2)
-
-        print('...완료')
-
+            
+    except ValueError as e:
+        print("ValueError:", e)
     except Exception as e:
-        print(e)
+        print("Exception:", e)
 
 sleep(2)        
 print('[데이터 수집 완료]\n소요 시간 :', time.time() - start)
 driver.quit()  # 작업이 끝나면 창을 닫는다.
 
-
-for name_list in names:
-    print(name_list)
+# json 파일로 저장
+with open('/Users/kim/Desktop/youngmi/data_test.json', 'w', encoding='utf-8') as f:
+    json.dump(parking_dict, f, indent=4, ensure_ascii=False)
